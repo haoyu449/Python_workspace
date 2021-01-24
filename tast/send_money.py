71e10123170bedb26af7853518492de6c4c88a05
haoyu449 <hhy449559@163.com>
2021-01-24 16:36:14 +0800

    删除了python_request

--- tast/send_money.py
+++
@@ -1,7 +0,0 @@
-# 定义发工资模块 send_money，用来增加收入计算
-
-from tast import money
-
-
-def send_money():
-    money.save_money = 2000
--- tast/start.py
+++
@@ -1,7 +0,0 @@
-# 定义一个start.py ，启动文件展示最终存款金额
-from tast.select_money import select_money
-from tast.send_money import send_money
-
-if __name__ == '__main__':
-    send_money()
-    select_money()
